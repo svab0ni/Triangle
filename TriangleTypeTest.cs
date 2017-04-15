74382c0d6d352573e0c708d3671534238c459dea
Ermin Šabotić <ermin-lol@hotmail.com>
2017-04-15 17:40:28 +0200

    Triangle Type Test added
    Uploading testing class that will be used to test the TriangleType located in class Triangle.
    TDD will be used  as development method.

---
+++ TriangleTypeTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleHomework;
+
+namespace TriangleUnitTest
+{
+    [TestClass]
+    public class TriangleTypeTest
+    {
+        [TestMethod]
+        public void incorrectSideLengths()
+        {
+            Triangle obj = new Triangle();
+            Assert.AreEqual("Wrong length of sides", obj.triangleType(-1, 2, 3));
+        }
+
+        [TestMethod]
+        public void triangleInequality()
+        {
+            Triangle obj = new Triangle();
+            Assert.AreEqual("Impossible to form Triangle with given sides", obj.triangleType(3,4,1));
+        }
+
+        [TestMethod]
+        public void equilateralTriangle()
+        {
+            Triangle obj = new Triangle();
+            Assert.AreEqual("Triangle is Equilateral", obj.triangleType(8, 8, 8));
+        }
+
+        [TestMethod]
+        public void isoscalesTriangle()
+        {
+            Triangle obj = new Triangle();
+            Assert.AreEqual("Triangle is Isoscales", obj.triangleType(3, 2, 3));
+        }
+
+        [TestMethod]
+        public void scaleneTriangle()
+        {
+            Triangle obj = new Triangle();
+            Assert.AreEqual("Triangle is Scalene", obj.triangleType(3,4,5));
+        }
+    }
+}
