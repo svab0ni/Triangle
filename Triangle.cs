391b9c78bd331243f928cdf1b4987b01764c67ec
Ermin Šabotić <ermin-lol@hotmail.com>
2017-04-15 18:47:00 +0200

    Triangle
    Added conditions if a triangle is acute, obtuse or right. 
    With this we have covered all needed test, and they all pass
    https://gyazo.com/ee74448217f8bbd14b5f18086ec66b37 - test results

--- Triangle.cs
+++ Triangle.cs
@@ -42,7 +42,33 @@ namespace TriangleHomework
 
         public string triangleTypeByAngle(int a, int b, int c)
         {
-            return "";
+            //check if any of the sides is smaller or equal to 0
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return "Wrong length of sides";
+            }
+
+            //Checking triangle ineqaulity. 
+            //can we actually form triangle with the given sides.
+            if (a + b <= c || b + c <= a || a + c <= b)
+            {
+                return "Impossible to form Triangle with given sides";
+            }
+
+            //checks if a triangle is right
+            if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b)
+            {
+                return "Triangle is right";
+            }
+            //checks if triangle is obtuse
+            if (a * a > b * b + c * c || b * b > a * a + c * c || c * c > a * a + b * b)
+            {
+                return "Triangle is obtuse";
+            }
+
+            //if triangle sides passed all criterias and 
+            //if triangle is neither of the types listed before it is acute
+            return "Triangle is acute";
         }
     }
 }
