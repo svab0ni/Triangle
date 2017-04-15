ee346e0c1be4e1029e20c93a5662601a526af9b7
Ermin Šabotić <ermin-lol@hotmail.com>
2017-04-15 17:50:19 +0200

    Triangle
    Added first condition to satisfy incorrectSideLengths() test
    https://gyazo.com/f842705f88364243fb37ee7e58c04b23 - after running all tests - no unexpected results

--- Triangle.cs
+++ Triangle.cs
@@ -10,6 +10,12 @@ namespace TriangleHomework
     {
         public string triangleType(int a, int b, int c)
         {
+            //check if any of the sides is smaller or equal to 0
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return "Wrong length of sides";
+            }
+
             return "";
         }
     }
